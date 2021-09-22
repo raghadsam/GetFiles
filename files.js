85394eb0df6e3a46d89cc88f57d0211c6e00f0aa
raghadsam <raghad66samrout@gmail.com>
2021-09-22 18:53:10 +0300

    added colors

--- files.js
+++ files.js
@@ -1,18 +1,19 @@
 var find = require("find");
+const chalk = require("chalk");
 
 //Total files
 find.file(__dirname, function (files) {
-  console.log("\nTotal files: " + files.length);
+  console.log(chalk.red("\nTotal files: " + files.length));
 });
 
 //All files names
 find.file(__dirname, function (filesName) {
   filesName.forEach((element) => {
-    console.log(element);
+    console.log(chalk.green(element));
   });
 });
 
 //JavaScript files
 find.file(/\.js$/, __dirname, function (JSfiles) {
-  console.log("\nJavaScript files: " + JSfiles.length);
+  console.log(chalk.underline.bgBlue("\nJavaScript files: " + JSfiles.length));
 });
